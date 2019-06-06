bce5e680e83730893d574cfc406972b83b2697ff
Tomas Harkema <tomas@harkema.io>
2019-06-06 10:20:59 +0200

    fix name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-cancellationtoken",
+    name: "CancellationToken",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "swift-cancellationtoken",
-            targets: ["swift-cancellationtoken"]),
+            name: "CancellationToken",
+            targets: ["CancellationToken"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swift-cancellationtoken",
+            name: "CancellationToken",
             path: "Sources"),
         .testTarget(
-            name: "swift-cancellationtokenTests",
-            dependencies: ["swift-cancellationtoken"],
+            name: "CancellationTokenTests",
+            dependencies: ["CancellationToken"],
             path: "Tests"),
     ]
 )
