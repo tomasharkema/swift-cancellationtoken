c3d7e7b4bc5167c4983c6cc128526cb2b536d86d
Tomas Harkema <tomas@harkema.io>
2019-06-06 10:19:08 +0200

    fix library in package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,29 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "CancellationToken",
-  targets: [
-    .target(name: "CancellationToken"),
-  ]
+    name: "swift-cancellationtoken",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "swift-cancellationtoken",
+            targets: ["swift-cancellationtoken"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "swift-cancellationtoken",
+            path: "Sources"),
+        .testTarget(
+            name: "swift-cancellationtokenTests",
+            dependencies: ["swift-cancellationtoken"],
+            path: "Tests"),
+    ]
 )
-
